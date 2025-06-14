7b77777db828f61df953663c53a3fa01f1735751
John Mike Asuncion <johnmikeasuncion17@gmail.com>
2025-06-14 16:39:21 +0800

    feat: add print non zero ending numbers oop

---
+++ basic_arithmetic_and_logic_operations_oop/print_non_zero_ending_numbers_oop/print_non_zero_ending_numbers_main.py
@@ -0,0 +1,10 @@
+from print_non_zero_ending_numbers_class import PrintNonZeroEndingNumbers
+
+class Main:
+    def execute(self):
+        exercise = PrintNonZeroEndingNumbers()
+        exercise.execute()
+
+if __name__ == "__main__":
+    main = Main()
+    main.execute()

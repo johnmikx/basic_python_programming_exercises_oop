05573174a71eeb7551b5c75b8a8e507678981e43
John Mike Asuncion <johnmikeasuncion17@gmail.com>
2025-06-14 16:58:49 +0800

    feat: add calculate integer quotient oop

---
+++ additional_operations_and_control_flow_oop/calculate_integer_quotient_oop/calculate_integer_quotient_main.py
@@ -0,0 +1,10 @@
+from calculate_integer_quotient_class import CalculateIntegerQuotient
+
+class Main:
+    def execute(self):
+        exercise = CalculateIntegerQuotient()
+        exercise.execute()
+
+if __name__ == "__main__":
+    main = Main()
+    main.execute()

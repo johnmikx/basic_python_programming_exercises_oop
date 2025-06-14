087b44034db5e93236e154b984d2ee1d3f03e38b
John Mike Asuncion <johnmikeasuncion17@gmail.com>
2025-06-14 17:22:23 +0800

    feat: add print non zero five ending numbers oop

---
+++ additional_operations_and_control_flow_oop/print_non_zero_five_ending_numbers_oop/print_non_zero_five_ending_numbers_class.py
@@ -0,0 +1,14 @@
+class PrintNonZeroFiveEndingNumbers:
+    def __init__(self):
+        self.numbers = []
+
+    def generate_numbers(self):
+        self.numbers = [num for num in range(0, 101) if num % 5 != 0]
+
+    def display_output(self):
+        print("Numbers from 0 to 100 except those ending in zero or five:")
+        print(*self.numbers)
+
+    def execute(self):
+        self.generate_numbers()
+        self.display_output()
---
+++ additional_operations_and_control_flow_oop/print_non_zero_five_ending_numbers_oop/print_non_zero_five_ending_numbers_main.py
@@ -0,0 +1,10 @@
+from print_non_zero_five_ending_numbers_class import PrintNonZeroFiveEndingNumbers
+
+class Main:
+    def execute(self):
+        exercise = PrintNonZeroFiveEndingNumbers()
+        exercise.execute()
+
+if __name__ == "__main__":
+    main = Main()
+    main.execute()

041be503b6a7d10ddff2a895dcadbc8d1c7cc35c
John Mike Asuncion <johnmikeasuncion17@gmail.com>
2025-06-14 17:08:00 +0800

    feat: check not equal numbers oop

---
+++ additional_operations_and_control_flow_oop/check_not_equal_numbers_oop/check_not_equal_numbers_main.py
@@ -0,0 +1,10 @@
+from check_not_equal_numbers_class import CheckNotEqualNumbers
+
+class Main:
+    def execute(self):
+        exercise = CheckNotEqualNumbers()
+        exercise.execute()
+
+if __name__ == "__main__":
+    main = Main()
+    main.execute()

62457f5ec0224e3d4960a4f30ebf6cbca5b04f66
John Mike Asuncion <johnmikeasuncion17@gmail.com>
2025-06-14 15:33:45 +0800

    feat: add check equal numbers oop

---
+++ basic_arithmetic_and_logic_operations_oop/check_equal_numbers_oop/check_equal_numbers_class.py
@@ -0,0 +1,20 @@
+class CheckEqualNumbers:
+    def __init__(self):
+        self.num1 = 0.0
+        self.num2 = 0.0
+        self.result = ""
+
+    def get_input(self):
+        self.num1 = float(input("Enter first number: "))
+        self.num2 = float(input("Enter second number: "))
+
+    def check(self):
+        self.result = "Equal" if self.num1 == self.num2 else "Not Equal"
+
+    def display_output(self):
+        print(self.result)
+
+    def execute(self):
+        self.get_input()
+        self.check()
+        self.display_output()
---
+++ basic_arithmetic_and_logic_operations_oop/check_equal_numbers_oop/check_equal_numbers_main.py
@@ -0,0 +1,10 @@
+from check_equal_numbers_class import CheckEqualNumbers
+
+class Main:
+    def execute(self):
+        exercise = CheckEqualNumbers()
+        exercise.execute()
+
+if __name__ == "__main__":
+    main = Main()
+    main.execute()

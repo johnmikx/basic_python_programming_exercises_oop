1f07cc5ab4d51bd48181647ca956c44e0e478be5
John Mike Asuncion <johnmikeasuncion17@gmail.com>
2025-06-14 15:37:23 +0800

    feat: add count odd numbers oop

---
+++ basic_arithmetic_and_logic_operations_oop/count_odd_numbers_oop/count_odd_numbers_main.py
@@ -0,0 +1,10 @@
+from count_odd_numbers_class import CountOddNumbers
+
+class Main:
+    def execute(self):
+        exercise = CountOddNumbers()
+        exercise.execute()
+
+if __name__ == "__main__":
+    main = Main()
+    main.execute()

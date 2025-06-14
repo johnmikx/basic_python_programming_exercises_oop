925c21a05f6662887333e50d52f7fd4768651fa9
John Mike Asuncion <johnmikeasuncion17@gmail.com>
2025-06-14 15:09:15 +0800

    feat: add calculate decimal quotient oop

---
+++ basic_arithmetic_and_logic_operations_oop/calculate_decimal_quotient_oop/calculate_decimal_quotient_class.py
@@ -0,0 +1,23 @@
+class CalculateDecimalQuotient:
+    def __init__(self):
+        self.num1 = 0.0
+        self.num2 = 0.0
+        self.quotient = None
+
+    def get_input(self):
+        self.num1 = float(input("Enter first number: "))
+        self.num2 = float(input("Enter second number: "))
+
+    def calculate(self):
+        if self.num2 == 0:
+            self.quotient = "undefined"
+        else:
+            self.quotient = self.num1 / self.num2
+
+    def display_output(self):
+        print(f"The quotient is: {self.quotient}")
+
+    def execute(self):
+        self.get_input()
+        self.calculate()
+        self.display_output()
---
+++ basic_arithmetic_and_logic_operations_oop/calculate_decimal_quotient_oop/calculate_decimal_quotient_main.py
@@ -0,0 +1,10 @@
+from calculate_decimal_quotient_class import CalculateDecimalQuotient
+
+class Main:
+    def execute(self):
+        exercise = CalculateDecimalQuotient()
+        exercise.execute()
+
+if __name__ == "__main__":
+    main = Main()
+    main.execute()
